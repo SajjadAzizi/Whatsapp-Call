f73d206c02a623459dc655a8f8067661de5f6208
Sajjad Azizi <sajad.azizi90@gmail.com>
2023-04-24 11:37:02 +0330

    update code

--- whatsapp-call.sh
+++ whatsapp-call.sh
@@ -1,20 +1,23 @@
+#!/bin/bash
 sudo apt update -y && apt upgrade -y 
-sudo apt install touch
-sudo apt install screen 
-sudo apt install lsof
+sudo apt install touch -y
+ sudo apt install lsof -y
+sudo apt install screen -y
 
 sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/daybreakersx/premscript/master/badvpn-udpgw64"
 sudo touch /etc/rc.local
-sudo nano /etc/rc.local
 
-----------------------
+cat > /etc/rc.local << ENDOFFILE
 #!/bin/sh -e
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7333
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7313 --max-clients 999
 exit 0
-----------------------
+ENDOFFILE
+
+
 
 chmod +x /etc/rc.local
-sudo systemctl status rc-local.service
+sudo systemctl restart rc-local.service
 sudo chmod +x /usr/bin/badvpn-udpgw
-sudo screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7333
+sudo screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7313 --max-clients 999
+
 reboot
